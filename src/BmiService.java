88d6b9eb63de0a78a8737d4566108fe43421ffe8
Nikolai Pustovoit <nikolaikop@gmail.com>
2021-10-05 20:01:31 -0600

    service only servvice

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,13 +1,6 @@
-import java.util.Scanner;
-
 public class BmiService {
     public double calculate() {
-        Scanner console = new Scanner(System.in);
-        System.out.println("Введите ваш вес в кг");
-        double weight = console.nextDouble();
-        System.out.println("Введите ваш рост в см");
-        double height = console.nextDouble() / 100;
-        double index = weight / Math.pow(height, 2);
+        double index = Main.weight / Math.pow(Main.height, 2);
         return index;
     }
 }
